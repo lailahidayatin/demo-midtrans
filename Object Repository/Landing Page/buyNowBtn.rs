8676926dd7964c8dbf879174f7aad71379bad4ce
Laila_Hidayatn <lhidayatin@gmail.com>
2022-07-02 22:05:04 +0700

    initiati object landing page & global variable

---
+++ Object Repository/Landing Page/buyNowBtn.rs
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>buyNowBtn</name>
+   <tag></tag>
+   <elementGuidId>2ed12d9c-1178-4525-9d9f-05accdb8edea</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@class = 'btn buy']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>btn buy</value>
+      <webElementGuid>1f6f2382-5e0e-494e-8cbb-faa90bef1dad</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
