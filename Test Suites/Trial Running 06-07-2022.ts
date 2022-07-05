aea33ed210a3536d0c9247bac7861c25951e949f
Laila_Hidayatn <lhidayatin@gmail.com>
2022-07-06 00:27:49 +0700

    WIP:test case DM5

---
+++ Test Suites/Trial Running 06-07-2022.ts
@@ -0,0 +1,41 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Trial Running 06-07-2022</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>false</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>015e0c44-2d4c-4abf-8335-7b74e4dba1fd</testSuiteGuid>
+   <testCaseLink>
+      <guid>d48659e2-8167-4261-8c53-567362dca451</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Success/DM4 - Sukses checkout dengan Promo installment</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>de539d9b-4049-4070-9251-cb8c41b1fc84</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Success/DM8 - Sukses checkout dengan Promo Proceed without promo</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5e4db882-5e55-4a89-9ba8-5400b7cb58e8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Success/DM9 - Sukses checkout dengan BANK BCA</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>dc333b2b-0e45-49ed-9e1f-a963061c9890</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
