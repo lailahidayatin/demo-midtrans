8a37be0020793a2f79ae90d426624b8aecc99efa
Belvatd <belva_talitha_28rpl@telkomschools.sch.id>
2022-07-06 22:09:25 +0700

    change int promo DM6 DM7

--- Test Suites/Trial Running 06-07-2022.ts
+++ Test Suites/Trial Running 06-07-2022.ts
@@ -12,21 +12,42 @@
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>015e0c44-2d4c-4abf-8335-7b74e4dba1fd</testSuiteGuid>
    <testCaseLink>
-      <guid>d48659e2-8167-4261-8c53-567362dca451</guid>
+      <guid>474c89bd-08e5-43b7-971c-35968a86413f</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Success/DM4 - Sukses checkout dengan Promo installment</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>de539d9b-4049-4070-9251-cb8c41b1fc84</guid>
+      <guid>3a9ff1ca-0d00-48b3-8d1b-53518885d0b4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Success/DM5 - Sukses checkout dengan Promo potongan 10</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>fbb2f1cc-467d-4cb1-89e9-d2416fc5fd9a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Success/DM6 - Sukses checkout dan gagal menggunakan Promo Midtrans karena kuota habis</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>72645048-a90e-4412-9da2-ae65ebd89ae9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Success/DM7 - Sukses checkout dengan Promo potongan 10 rupiah</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8262f92e-7343-4a10-a283-dc09291ea91e</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Success/DM8 - Sukses checkout dengan Promo Proceed without promo</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>5e4db882-5e55-4a89-9ba8-5400b7cb58e8</guid>
+      <guid>5d772228-7d73-47eb-9f2a-db63c1effe7e</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Success/DM9 - Sukses checkout dengan BANK BCA</testCaseId>
@@ -37,5 +58,12 @@
          <value></value>
          <variableId>dc333b2b-0e45-49ed-9e1f-a963061c9890</variableId>
       </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c78d9d44-738f-4edf-b33b-98c039a7e43e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Fail/DM1 - Gagal checkout dengan invalid data pada credit and debit card</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
